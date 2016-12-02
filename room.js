62366353ce43ac1912f4f3c39b8f4bac56566952
Jim Davis <jrd3@alum.mit.edu>
2016-12-01 21:00:01 -0500

    Just adding comments to methods

--- room.js
+++ room.js
@@ -10,7 +10,7 @@ function Room(id, brief, description) {
 	this.state = {};
 }
 
-// describe from player's perspective
+// return the string that describes the room from player's perspective
 Room.prototype.describe = function (context) {
 	var s = "You are " + this.preposition() + " " + this.get_description(context);
 	if (this.contents.length > 0) {
@@ -25,6 +25,8 @@ Room.prototype.describe = function (context) {
 	return s;
 };
 
+// normally this is just the static description of the room
+// but it may be state dependent.
 Room.prototype.get_description = function (context) {
 	return this.description;
 };
@@ -37,33 +39,34 @@ Room.prototype.set_state = function (s, v) {
 	this.state[s]=v;
 };
 
-
+// the arcs leading out that are currently visible
 Room.prototype.visible_exits = function (context) {
 	return _.filter(this.arcs, a => a.is_visible(context));
 };
 
-Room.prototype.preposition = function () {
-	return this.supporter ? "on" : "in";
-};
 
 Room.prototype.add_arc = function (arc) {
 	this.arcs[arc.direction]=arc;
 };
 
+// find the arc, either by direction or by the noun of its name
 Room.prototype.has_arc = function (direction) {
 	return this.arcs[direction] ||
 		_.find(this.arcs, a => a.match(direction));
 };
 
+// true iff the noun is in the room
 Room.prototype.has_item = function (noun) {
 	return this.contents.indexOf(noun) >= 0;
 };
 
+// add a noun.  Return the room so we can chain methods
 Room.prototype.add_item = function (noun) {
 	this.contents.push(noun);
 	return this;
 };
 
+// remove the noun.  It is an error to call this if not thee
 Room.prototype.remove_item = function (noun) {
 	var idx = this.contents.indexOf(noun);
 	if (idx < 0) {
@@ -74,11 +77,22 @@ Room.prototype.remove_item = function (noun) {
 	}
 }
 
+// If a room is a "supporter" it supports the player, so
+// we say you are "on" the room not "in" the room.
+// So "on the stairs", "on the sidewalk", but "in the living room"
 Room.prototype.set_supporter = function (b) {
 	this.supporter = b;
 	return this;
 };
 
+// the preposition to use when describing the players
+// relation to the room: usually it is "in" but it could be "on"
+Room.prototype.preposition = function () {
+	return this.supporter ? "on" : "in";
+};
+
+
+// the the Noun in the room
 Room.prototype.find = function (np) {
 	return _.find(this.contents, n => n.match(np));
 };
